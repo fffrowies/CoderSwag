2e0b676bdecb34b55139c78fd79100b397b5954d
fffrowies <f3rnandorowies.saxo@gmail.com>
2018-12-05 13:12:42 -0300

    Refactor for recycler view adapter

--- app/src/main/java/com/example/fr/coderswag/Controller/MainActivity.kt
+++ app/src/main/java/com/example/fr/coderswag/Controller/MainActivity.kt
@@ -2,20 +2,25 @@ package com.example.fr.coderswag.Controller
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import com.example.fr.coderswag.Adapters.CategoryAdapter
+import android.support.v7.widget.LinearLayoutManager
+import com.example.fr.coderswag.Adapters.CategoryRecycleAdapter
 import com.example.fr.coderswag.R
 import com.example.fr.coderswag.Services.DataService
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var adapter: CategoryAdapter
+    lateinit var adapter: CategoryRecycleAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        adapter = CategoryAdapter(this, DataService.categories)
+        adapter = CategoryRecycleAdapter(this, DataService.categories)
         categoryListView.adapter = adapter
+
+        val layoutManager = LinearLayoutManager(this)
+        categoryListView.layoutManager = layoutManager
+        categoryListView.setHasFixedSize(true)
     }
 }
--- app/src/main/java/com/example/fr/coderswag/Services/DataService.kt
+++ app/src/main/java/com/example/fr/coderswag/Services/DataService.kt
@@ -8,6 +8,14 @@ object DataService {
         Category("SHIRTS", "shirtimage"),
         Category("HOODIES", "hoodieimage"),
         Category("HATS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage"),
+        Category("PANCHOS", "shirtimage"),
+        Category("CAHORRITOS", "hoodieimage"),
+        Category("JALAPEÑOS", "hatimage"),
+        Category("DIGITAL", "digitalgoodsimage"),
+        Category("SHIRTS", "shirtimage"),
+        Category("HOODIES", "hoodieimage"),
+        Category("HATS", "hatimage"),
         Category("DIGITAL", "digitalgoodsimage")
     )
 
